08e9eee9e1d40d6c2d8a085413c07c8bab824d60
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2022-05-04 12:05:26 -0700

    Time: 2 ms (29.79%), Space: 59 MB (6.38%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+     public void reverseString(char[] s) {
+        helper(s, 0, s.length - 1);
+    }
+
+    public void helper(char[] s, int left, int right) {
+        if (left >= right) return;
+        
+        char tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+        
+        helper(s, left, right);
+    }
+}
