c09e2a9743843674e6147f2d5e0d1703595ac6aa
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2022-05-05 11:02:36 -0700

    Time: 0 ms, Memory: 45.7 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -12,14 +12,14 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
-       if(head==null) return false;
     ListNode walker = head;
-    ListNode runner = head;
-    while(runner.next!=null && runner.next.next!=null) {
-        walker = walker.next;
-        runner = runner.next.next;
-        if(walker==runner) return true;
-    }
-    return false;
+        ListNode runner = head;
+        
+        while (runner != null && runner.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
     }
 }
