5f6d22a0104beb9ba4189c99b4597b529b936332
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2022-07-12 21:17:15 -0700

    Time: 6 ms (6.54%), Space: 41.3 MB (77.82%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -16,4 +16,25 @@ class Solution {
 
         return sum; 
     }
+    
+    
+    public int sumOfUnique2(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        if (nums.length == 1) return nums[0];
+        if (nums[0] == nums[nums.length - 1]) return 0;
+
+        int count = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                sum += nums[i];
+                count = 1;
+            } else {
+                count++;
+                if (count <= 2) sum -= nums[i + 1];
+            }
+        }
+        return sum + nums[nums.length - 1];
+    }
 }
