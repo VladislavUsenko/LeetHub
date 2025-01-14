cfbdb7bde2294930dff875c4f4070e09f4a18f0a
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2025-01-13 21:13:15 -0700

    add 459 task

---
+++ 459-repeated-substring-pattern/Solution.java
@@ -0,0 +1,11 @@
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(repeatedSubstringPattern("abcabcabcabc"));
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+        return false;
+    }
+}
