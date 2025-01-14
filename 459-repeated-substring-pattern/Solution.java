d0def303122d5d4f919806250db756f0ba421efb
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2025-01-13 21:20:23 -0700

    solve 459-repeated-substring-pattern

--- 459-repeated-substring-pattern/Solution.java
+++ 459-repeated-substring-pattern/Solution.java
@@ -6,6 +6,20 @@ class Solution {
     }
 
     public static boolean repeatedSubstringPattern(String s) {
-        return false;
+
+        int len = s.length();
+    	for(int i = len / 2 ; i >= 1 ; i--) {
+    		if(len % i != 0) continue;
+
+    		int m = len / i;
+    		String subS = s.substring(0,i);
+    		int j;
+    		for(j = 1; j < m; j++) {
+    			if(!subS.equals(s.substring(j * i, i + j * i))) break;
+    		}
+    		if(j == m) return true;
+    		
+    	}
+    	return false;
     }
 }
