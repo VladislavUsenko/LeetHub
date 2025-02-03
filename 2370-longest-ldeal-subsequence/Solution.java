d91a2701e48ce97ab9a8f73ed1297e042b3b204e
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2025-02-02 23:32:38 -0700

    refactor lognest ideal subsequence

--- 2370-longest-ldeal-subsequence/Solution.java
+++ 2370-longest-ldeal-subsequence/Solution.java
@@ -5,7 +5,7 @@ class Solution {
     public static void main(String[] args) {
         String s = "jxhwaysa";
 
-        System.out.println("---------------");
+        System.out.println("++++++++++++++++");
         System.out.println(longestIdealString(s, 14) );
     }
 
