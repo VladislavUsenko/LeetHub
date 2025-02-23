59c8b08fc7a6191bd266406b7b0109a178895594
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2025-02-22 23:09:43 -0700

    solved 2965 firnd missing and repeated

---
+++ 2965-find-missing-and-repeated/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static void main(String[] args) {
+        
+        int[][] grid = {
+            {9, 1, 7},
+            {8, 9, 2},
+            {3, 4, 6}
+        };
+
+        System.out.println("=============");
+        System.out.println(
+            Arrays.toString(
+                findMissingAndRepeatedValues(grid)
+            )
+        );
+
+
+    }
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+
+        int n = grid.length;
+        
+        int[] map = new int[n * n + 1];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                map[grid[i][j]]++;
+            }
+        }
+
+        int[] res = new int[2];
+
+        System.out.println(Arrays.toString(map));
+
+        for (int i = 1; i < map.length; i++) {
+            if (map[i] == 0) res[1] = i;
+            if (map[i] == 2) res[0] = i;
+        }
+
+        return res;
+    }
+}
