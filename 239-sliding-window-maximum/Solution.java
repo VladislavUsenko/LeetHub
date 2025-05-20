222ae0b2883cb31a73b963f9f13c2ee7a49d64bc
Vladislav Ussenko <Usenko.Vladislav.i@gmail.com>
2025-05-19 21:07:31 -0600

    solved 239 sliding window maximum in Java

---
+++ 239-sliding-window-maximum/Solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+
+        int[] res = maxSlidingWindow(nums, k);
+
+        System.out.println(Arrays.toString(res));
+    }
+
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) return new int[0];
+        
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i = 0; i < n; i++) {
+            
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+    
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return res;
+    }
+}
